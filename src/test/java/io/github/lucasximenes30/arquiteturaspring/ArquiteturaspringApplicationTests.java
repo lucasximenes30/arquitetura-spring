5c2d84a47c170131d459ca8492c8a7f45316470f
Lucas Ximenes <lucasximenes177@gmail.com>
2025-05-29 20:29:21 -0300

    first commit

---
+++ src/test/java/io/github/lucasximenes30/arquiteturaspring/ArquiteturaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lucasximenes30.arquiteturaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiteturaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
